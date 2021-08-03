09ce6eba524848bd9f144512991b21b4f6884478
Qamar Alkhatib <qalkhatib0@gmail.com>
2021-08-03 15:46:22 +0300

    adding JS

--- JS/app.js
+++ JS/app.js
@@ -92,8 +92,65 @@ document.write("Nice to see you here " + username + "!");
                         alert("You shoud answer the question with yes or no!");
                         break;
                    }
-                    alert(' Thank you for playing this game ' + username + '  your score from 5 is  '+ score);
-          
+
+                   let sixthAnswer = Number(prompt("Sixth Question: How old am I? ",'numbers only'))
+                   let attempts = 4;
+                   while (attempts) {
+                    for (let i = 0; i <= attempts; i++) {
+                        if (sixthAnswer > 25) {
+                            alert('You are too high!');
+                            break;
+                        } else if (sixthAnswer < 15) {
+                            alert('You are too low!');
+                            break;
+                        } 
+                        else if (sixthAnswer == 19) {
+                            alert('you are to close!');
+                            break;
+                        }
+                        else if (sixthAnswer == 20) {
+                            alert('you are Correct!');
+                            break;
+                        } 
+                        else {
+                            alert('You are Wrong!');
+                            break;
+                        }
+                    }
                     
+                   attempts -= 1;
+                      if (sixthAnswer == 20) {
+                      break;
+                      }
+
+                 sixthAnswer = prompt('Please try again you have: ' + attempts + ' Only');
+            }
+                 if (sixthAnswer != 20) {
+              alert('You didnt guessed');
+              alert('The correct answer was 20 ');
+             }
+
+
+                    let guessAct = ['gym', 'eating', 'watching my classes', 'music'];
+                    let sevAnswer = Number(prompt("Seventh Question: Guess my favorite activity in my day, You have only 6 attempts"))
+                    
+                    for (let j = 1; j<= 6; j++){
+                         for(let y = 0; y < guessAct.length; y++){
+                               if(sevAnswer.toLowerCase() === guessAct[y].toLowerCase){
+                                alert('Great!, You guessed')
+                                break;
+                               } 
+                               else{
+                                   alert("Try again")
+                               }
+                            }
+                            if(sevAnswer.toLowerCase() !== guessAct[y].toLowerCase){
+
+                                alert('The correct answer was one of those Gym, music, eating');
+                            }
+                                       
+                        
+                    }
+
+                    alert(' Thank you for playing this game ' + username + '  your score from 7 is  '+ score);
                 
-        
