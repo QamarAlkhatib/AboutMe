5ae15404bcc6f82e267039738774bfbf51ee58e3
Bzour97 <x_a@outlook.com>
2021-08-04 14:30:47 +0300

    Ahmad Bzour

--- JS/app.js
+++ JS/app.js
@@ -5,6 +5,8 @@ let username = prompt('What is your Name?');
 alert('welcome, ' + username);
 document.write('Nice to see you here ' + username + '!');
 
+function FirstQuestion(){
+
 let firstAnswer = prompt('First Question: Do you think Im a strange person? ', 'yes/no');
 
 switch(firstAnswer.toLowerCase()){
@@ -21,7 +23,11 @@ default:
     alert('You shoud answer the question with yes or no!');
     break;
 }
+}
 
+FirstQuestion();
+
+function SecondQuestion(){
 
 let SecAnswer = prompt('Second Question: Do you think I like coding? ', 'yes/no');
 switch(SecAnswer.toLowerCase()){
@@ -38,6 +44,11 @@ default:
     alert('You shoud answer the question with yes or no!');
     break;
 }
+}
+
+SecondQuestion();
+
+function ThirdQuestion(){
 
 let ThiAnswer = prompt('Third Question: Do you think I like debates?', 'yes/no');
 
@@ -55,6 +66,11 @@ default:
     alert('You shoud answer the question with yes or no!');
     break;
 }
+}
+
+ThirdQuestion();
+
+function FourthQuestion(){
 
 let foAnswer = prompt('fourth Question: Do you think I\'m an athletic person? ', 'yes/no');
 
@@ -71,6 +87,12 @@ default:
     alert('You shoud answer the question with yes or no!');
     break;
 }
+}
+
+FourthQuestion();
+
+function FifthQuestion(){
+
 let fivAnswer = prompt('fifth Question: Do you think Im a good reader? ', 'yes/no');
 
 
@@ -89,6 +111,11 @@ default:
     alert('You shoud answer the question with yes or no!');
     break;
 }
+}
+
+FifthQuestion();
+
+function SixthQuestion(){
 
 let sixthAnswer = Number(prompt('Sixth Question: How old am I? ','numbers only'));
 let attempts = 4;
@@ -128,9 +155,16 @@ if (sixthAnswer !== 20) {
     alert('The correct answer was 20 ');
 }
 
+}
+
+SixthQuestion();
+
+
+function QuestionSeven(){
 
 
 let activity = ['gym', 'eating', 'watching movie'];
+
 
 
 for (let i = 1; i <= 6; i++){
@@ -156,7 +190,11 @@ for (let i = 1; i <= 6; i++){
     }
 }
 
+}
+
+QuestionSeven();
 
 alert('my favorite activity was those: '+ activity);
 
 alert(' Thank you for playing this game ' + username + '  your score is '+ score + ' from 7');
+
